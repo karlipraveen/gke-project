6549fb9be0b4820452ed658deacbeb6206c0ec2d
karlipraveen <karlipraveen@gmail.com>
2026-07-28 18:24:47 +0530

    Remove unnecessary whitespace from firewall.tf

---
+++ terraform-jenkins/firewall.tf
@@ -0,0 +1,22 @@
+resource "google_compute_firewall" "jenkins_firewall" {
+  name = "jenkins-firewall"
+  network = var.network
+
+  allow {
+    protocol = "tcp"
+    ports = ["22"]
+  }
+
+  allow {
+    protocol = "tcp"
+    ports = ["80"]
+  }
+
+  allow {
+    protocol = "tcp"
+    ports = ["8080"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags = ["jenkins"]
+}
